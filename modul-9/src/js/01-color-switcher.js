fb56d5f7aeb2f58d7036208ff87fbf750d72911a
dobroillya <dobroillya@gmail.com>
2023-03-18 16:39:26 +0200

    promices

--- modul-9/src/js/01-color-switcher.js
+++ modul-9/src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+let idStart;
+
+function handleInterval() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function startRandom() {
+  idStart = setInterval(handleInterval, 1000);
+  start.disabled = true;
+}
+
+function stopRandom() {
+  clearInterval(idStart);
+  start.disabled = false;
+}
+
+start.addEventListener('click', startRandom);
+
+stop.addEventListener('click', stopRandom);
--- modul-9/src/js/03-promises.js
+++ modul-9/src/js/03-promises.js
@@ -1,8 +1,36 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector('.form');
+const step = document.querySelector('[name="step"]');
+const delay = document.querySelector('[name="delay"]');
+const amount = document.querySelector('[name="amount"]');
+
+function createPromise(e, position, delay) {
+  e.preventDefault();
+  console.log(position, delay);
+
+  for (let i = 0; i < amount.value; i++) {
+    let promise1 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const shouldResolve = Math.random() > 0.3;
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+
+    console.log(promise1);
+
+    promise1
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
+form.addEventListener('submit', (e, pos = amount.value, del = delay.value) =>
+  createPromise(e, pos, del)
+);
